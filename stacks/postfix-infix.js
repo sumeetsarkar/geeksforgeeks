758153d8a3cd6fff348fe7838e9c8739b4ad2cc5
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2017-12-05 11:05:12 +0530

    added eslint

--- stacks/postfix-infix.js
+++ stacks/postfix-infix.js
@@ -1,20 +1,21 @@
-const postfix = 'a b + c *'
+const postfix = 'a b + c *';
 const postfixArr = postfix.split(' ');
 const stack = [];
 const n = postfixArr.length;
 
 for (let i = 0; i < n; i++) {
   const item = postfixArr[i];
+  let first, second;
   switch(item) { 
-    case '+':
-    case '-':
-    case '*':
-    case '/':
-      const first = stack.pop();
-      const second = stack.pop();
-      stack.push(`${second} ${item} ${first}`);
-      break;
-    default: stack.push(item);
+  case '+':
+  case '-':
+  case '*':
+  case '/':
+    first = stack.pop();
+    second = stack.pop();
+    stack.push(`${second} ${item} ${first}`);
+    break;
+  default: stack.push(item);
   }
 }
 
