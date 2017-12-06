b102de4004cbfc49ee64586cd284f7a5584a0998
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2017-12-06 12:17:16 +0530

    arrays: reverse sub arrays

---
+++ arrays/reverse-groups.js
@@ -0,0 +1,28 @@
+const arr = [19, 3, 56, 8, 21, 5, 30];
+const n = arr.length;
+
+const g = 2; // group size
+
+let p = 0;
+let i = 0;
+let j = g - 1;
+const possibleWholeGroups = parseInt(n/g);
+
+while (p < possibleWholeGroups) {
+  reverseByIteration(arr, i, j);
+  i += g;
+  j += g;
+  p++;
+}
+
+console.log(arr);
+
+function reverseByIteration(arr, i, j) {
+  while (i < j) {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    i++;
+    j--;
+  }
+}
